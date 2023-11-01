35564dba286b23a99dee0523f3c7a641b7016d8f
thevalter <valternunes776@gmail.com>
2023-11-01 07:21:13 -0300

    update: change fields name

--- api/updateProduct.js
+++ api/updateProduct.js
@@ -2,11 +2,11 @@ const pool = require('./config');
 
 const updateProduct = (request, response) => {
     const id = parseInt(request.params.id);
-    const { product, brand, price, amount } = request.body;
+    const { film, brand, price, amount } = request.body;
 
     pool.query(
         'UPDATE products SET product = $1, brand = $2, price = $3, amount = $4 WHERE id = $5',
-        [product, brand, price, amount, id],
+        [film, brand, price, amount, id],
         (error, results) => {
             if (error) {
                 throw error;
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
